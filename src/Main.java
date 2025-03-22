7c3f8adb0fe17b80454e5c6e2c02a5a526e859ab
lyays <l28075339@gmail.com>
2025-03-23 00:42:52 +0300

    hm8

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        // Задача 1.1
+        System.out.println ("Задача 1.1");
         int[] weight = new int[3];
         weight[0] = 1;
         weight[1] = 2;
@@ -10,23 +10,26 @@ public class Main {
         System.out.println(weight[0]);
         System.out.println(weight[1]);
         System.out.println(weight[2]);
-        // Задача 1.2
+        System.out.println ("Задача 1.2");
         double[] flowers = {1.57, 7.654, 9.986};
         System.out.println(flowers[0]);
         System.out.println(flowers[1]);
         System.out.println(flowers[2]);
-        // Задача 1.3
+        System.out.println ("Задача 1.3");
         int[] paints = {35, 9, 4, 0, 0, 0, 0, 0};
         for (int i = 0; i < 8; i++) {
             System.out.println(paints[i]);
         }
-        // Задача 2
+        System.out.println ("Задача 2");
         int[] candyBox = new int[5];
         candyBox[0] = 8;
         candyBox[4] = 2;
+        for (int i = 0; i < 5; i++) {
+        }
         System.out.println(Arrays.toString(candyBox));
 
-        // Задача 3.1
+
+        System.out.println ("Задача 3.1");
         int[] avengers = new int[3];
         avengers[0] = 1;
         avengers[1] = 2;
@@ -38,7 +41,7 @@ public class Main {
                 System.out.println(avengers[i] + " ");
             }
         }
-        //Задача 3.2
+        System.out.println ("Задача 3.2");
         double[] numbers = {1.57, 7.654, 9.986};
         for (int i = numbers.length - 1; i >= 0; i--) {
             if (i != 0) {
@@ -47,7 +50,7 @@ public class Main {
                 System.out.println(numbers[i] + " ");
             }
         }
-        // Задача 4
+        System.out.println ("Задача 4");
         int[] weightOne = new int[3];
         weightOne[0] = 1;
         weightOne[1] = 2;
